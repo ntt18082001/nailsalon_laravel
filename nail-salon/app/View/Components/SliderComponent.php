67fd9f14a0d748b18e54a9f373a9ffc9aa814b75
TienSi <ntt18082001@gmail.com>
2023-02-17 21:03:15 +0700

    Update logic slider

--- nail-salon/app/View/Components/SliderComponent.php
+++ nail-salon/app/View/Components/SliderComponent.php
@@ -3,6 +3,7 @@
 namespace App\View\Components;
 
 use App\Models\Slider;
+use Illuminate\Support\Facades\Date;
 use Illuminate\View\Component;
 
 class SliderComponent extends Component
@@ -24,7 +25,11 @@ class SliderComponent extends Component
      */
     public function render()
     {
-        $data = Slider::all();
+        $now = now()->toDateString();
+        $data = Slider::where('from', '=', null)->where('to', '=', null)
+            ->orWhere('from', '!=', null)->where('to', '=', null)->whereDate('from', '<=', $now)
+            ->orWhere('from', '=', null)->where('to', '!=', null)->whereDate('to', '>=', $now)
+            ->orWhere('from', '!=', null)->where('to', '!=', null)->whereDate('from', '<=', $now)->whereDate('to', '>=', $now)->get();
         return view('components.slider-component')->with('data', $data);
     }
 }
